0b6428320d2eee0106468336da26ea44d02b33c3
neagu robert alexandru <127115019+NeaguRobertAlexandru31@users.noreply.github.com>
2023-12-13 12:18:08 +0100

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarTopComponent } from './navbar-top/navbar-top.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarTopComponent,
   ],
   imports: [
     BrowserModule,
