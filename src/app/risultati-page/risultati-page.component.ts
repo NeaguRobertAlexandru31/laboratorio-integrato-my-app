2814e05051cbf221d8b3e19d84c277f14a1469ab
neagu robert alexandru <127115019+NeaguRobertAlexandru31@users.noreply.github.com>
2023-12-20 12:03:31 +0100

    update

--- src/app/risultati-page/risultati-page.component.ts
+++ src/app/risultati-page/risultati-page.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-risultati-page',
   templateUrl: './risultati-page.component.html',
   styleUrls: ['./risultati-page.component.scss']
 })
-export class RisultatiPageComponent {
+export class RisultatiPageComponent implements OnInit {
 
+  constructor(){}
+
+  ngOnInit(): void {
+    this.generateRows();
+  }
+
+  generateRows(): void{
+    const tableBody = document.getElementById("tableBody");
+    if(tableBody){
+      for(let i = 0; 1 < 10; i++){
+        const newRow = this.createRow();
+        tableBody.appendChild(newRow);
+      }
+    }
+  }
+
+  createRow(): HTMLTableRowElement{
+    const row = document.createElement("tr");
+    row.classList.add('w-full', 'h-28', 'flex', 'justify-evenly');
+
+    for (let i = 0; i < 3; i++) {
+      const cell = document.createElement('td');
+      cell.classList.add('w-1/3', 'h-10', 'text-center');
+      cell.textContent = 'Contenuto';
+      row.appendChild(cell);
+    }
+
+    return row;
+  }
 }
