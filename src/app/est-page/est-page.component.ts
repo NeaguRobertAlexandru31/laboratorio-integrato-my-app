edc020001f4cc72e1de92e5b499271572b0524b8
Er-Giuj <127399670+Er-Giuj@users.noreply.github.com>
2023-12-22 17:11:13 +0100

    messo a posto navbar,aggiustare logica stampa e la row contenente le tabs

--- src/app/est-page/est-page.component.ts
+++ src/app/est-page/est-page.component.ts
@@ -14,7 +14,7 @@ export class EstPageComponent implements OnInit {
 
   generateRows(): void {
     function createRow(
-      position: number,
+      position: string,
       nameTeam: string,
       gamePlayed: string,
       win: string,
@@ -26,32 +26,32 @@ export class EstPageComponent implements OnInit {
 
       const cell1 = document.createElement('td');
       cell1.classList.add('w-1/3', 'h-10', 'text-center');
-      // cell1.textContent = team1;
+      cell1.textContent = position;
       row.appendChild(cell1);
 
       const cell2 = document.createElement('td');
       cell2.classList.add('w-1/3', 'h-10', 'text-center');
-      // cell2.textContent = score;
+      cell2.textContent = nameTeam;
       row.appendChild(cell2);
 
       const cell3 = document.createElement('td');
       cell3.classList.add('w-1/3', 'h-10', 'text-center');
-      // cell3.textContent = team2;
+      cell3.textContent = gamePlayed;
       row.appendChild(cell3);
 
       const cell4 = document.createElement('td');
       cell1.classList.add('w-1/3', 'h-10', 'text-center');
-      // cell1.textContent = team1;
+      cell1.textContent = win;
       row.appendChild(cell4);
 
       const cell5 = document.createElement('td');
       cell2.classList.add('w-1/3', 'h-10', 'text-center');
-      // cell2.textContent = score;
+      cell2.textContent = lose;
       row.appendChild(cell5);
 
       const cell6 = document.createElement('td');
       cell3.classList.add('w-1/3', 'h-10', 'text-center');
-      // cell3.textContent = team2;
+      cell3.textContent = percWIn;
       row.appendChild(cell6);
 
       return row;
@@ -60,12 +60,20 @@ export class EstPageComponent implements OnInit {
     const tableBody = document.getElementById('tableBody');
     if (tableBody) {
        for (let i = 0; i < 10; i++) {
-         const newRow = createRow();
+         const newRow = createRow(
+          `N ${i+1}`,
+          `NameSquad ${i + 3}`,
+          `PG ${i + 4}`,
+          `WIN ${i + 5}`,
+          `LOSE ${i + 6}`,
+          `% ${i + 6}`,
+         );
        tableBody.appendChild(newRow);
     }
   }
 }
-
+}
+  
 // for (let i = 0; i < 10; i++) {
 //   const newRow = createRow(
 //     `Squadra ${i + 1}`,
