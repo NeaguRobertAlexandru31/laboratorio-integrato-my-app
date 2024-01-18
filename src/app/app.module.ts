59be4b3468ebffdfd154026ec151db48b2904868
R0b__zombie_31 <neagurobertalexandru@gmail.com>
2024-01-18 17:29:41 +0100

    update login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -82,6 +83,7 @@ import { FilterPipe } from './_hooks/filter.pipe';
     MatDatepickerModule,
     MatNativeDateModule,
     MatIconModule,
+    ReactiveFormsModule
   ],
   providers: [ApiService, { provide: DateTime, useValue: DateTime },],
   bootstrap: [AppComponent],
--- src/app/pages/accesso-page/accesso-page.component.ts
+++ src/app/pages/accesso-page/accesso-page.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-accesso-page',
   templateUrl: './accesso-page.component.html',
-  styleUrls: ['./accesso-page.component.scss']
+  styleUrls: ['./accesso-page.component.scss'],
 })
 export class AccessoPageComponent {
+  constructor() {}
 
-   
+  loginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
 
+  signin() {
+    const formData = {
+      email: this.loginForm.get('email')?.value,
+      password: this.loginForm.get('password')?.value,
+    };
+
+    fetch('http://localhost:8045/user/signin', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        console.log('Accesso avvenuto con successo:', response);
+        // Puoi aggiungere qui il reindirizzamento o altre azioni dopo l'accesso.
+      })
+      .catch((error) => {
+        console.error("Errore durante l'accesso:", error);
+      });
+  }
 }
