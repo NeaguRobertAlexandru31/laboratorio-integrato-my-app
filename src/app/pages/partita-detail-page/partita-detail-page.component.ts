392395cefb9ed38a78fac1ab62905d4c9629cf71
Ricksarbo <riccardo.sarboraria@gmail.com>
2024-01-21 22:27:41 +0100

    Update - Statistiche
    Inserita una funzione di calcolo per la lunghezza delle percentuali delle statistiche

--- src/app/pages/partita-detail-page/partita-detail-page.component.ts
+++ src/app/pages/partita-detail-page/partita-detail-page.component.ts
@@ -34,34 +34,39 @@ export class PartitaDetailPageComponent implements OnInit {
     this.isVisible = !this.isVisible;
   }
 
-  calcolaBackground(coloreA: string, coloreB: string) {
-    const A = 30; // Sostituisci con il tuo valore
-    const B = 70; // Sostituisci con il tuo valore
-    const lunghezzaTotale = 500; // Sostituisci con la tua lunghezza totale
-
-    const risultati = this.calcolaPercentuali(A, B, lunghezzaTotale);
-
-    // Costruisci la parte della stringa con le percentuali
-    const parteStringaA = `'B3 ${risultati.percentualeA.toFixed(2)}% ,'`;
-    const parteStringaB = `'B3 ${risultati.percentualeB.toFixed(2)}% ,'`;
-
-    return coloreA + parteStringaA + coloreB + parteStringaB + ')';
-  }
-
-  calcolaPercentuali(A:number, B:number, lunghezzaTotale:number) {
-    const percentualeA = (A / (A + B)) * 100;
-    const percentualeB = (B / (A + B)) * 100;
-
-    const lunghezzaA = (percentualeA / 100) * lunghezzaTotale;
-    const lunghezzaB = (percentualeB / 100) * lunghezzaTotale;
+  calcolaPercentuale(colore: string, a: number, b:number ) {
+    const sommaPercentuali = a + b;
 
     return {
-        percentualeA: percentualeA,
-        percentualeB: percentualeB,
-        lunghezzaA: lunghezzaA,
-        lunghezzaB: lunghezzaB
+      'flex': `0 0 ${(a / sommaPercentuali) * 100}%`,
+      'background-color': colore
     };
-}
+  }
+  // calcolaPercentuali(A:number, B:number, lunghezzaTotale:number) {
+  //   const percentualeA = (A / (A + B)) * 100;
+  //   const percentualeB = (B / (A + B)) * 100;
+
+  //   const lunghezzaA = (percentualeA / 100) * lunghezzaTotale;
+  //   const lunghezzaB = (percentualeB / 100) * lunghezzaTotale;
+
+  //   return {
+  //       percentualeA: percentualeA,
+  //       percentualeB: percentualeB,
+  //       lunghezzaA: lunghezzaA,
+  //       lunghezzaB: lunghezzaB
+  //   };
+  // }
+  
+
+  // calcolaBackground(A:number, B:number, coloreA: string, coloreB: string) {
+
+  //   const risultati = this.calcolaPercentuali(A, B, A+B);
+
+  //   const parteStringaA = `'${risultati.percentualeA.toFixed(2)}% ,'`;
+  //   const parteStringaB = `'${risultati.percentualeB.toFixed(2)}%'`;
+
+  //   return coloreA + parteStringaA + coloreB + parteStringaB + ')';
+  // }
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe((params) => {
