635576bdb39d553648c1fc3eca4b6a04d8f03499
Ricksarbo <riccardo.sarboraria@gmail.com>
2024-01-22 22:35:49 +0100

    Update - Blog
    Collegato il blog

--- src/app/pages/programmate-page/programmate-page.component.ts
+++ src/app/pages/programmate-page/programmate-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DateTime } from 'luxon';
 import Game from 'src/app/_models/game.model';
 import { ApiService } from 'src/app/_service/api.service';
 
@@ -9,28 +10,25 @@ import { ApiService } from 'src/app/_service/api.service';
 })
 export class ProgrammatePageComponent implements OnInit {
   nextGames:Game[] = []; 
+  currentDate: Date = new Date();
   
   constructor(private apiService: ApiService) {}
 
-  // //Programmate
-  // isLoadingNext: boolean = true; // Flag per indicare se le partite sono in fase di caricamento
-  // isLoadedNextGames:boolean = false;
-  // //Funzione di caricamento controlla lo stato della chiamata se restituisce o meno
-  // loadingPlayers(teamId: number) {
-  //   if(this.isLoadedNextGames == false){
-  //   this.isLoadingNext = true;
-  //   this.apiService.getNextGame(teamId,2023).subscribe({
-  //     next: (response) => {
-  //       this.nextGames = response;
-  //     },
-  //     error: (error) => console.error('Error fetching players', error),
-  //     complete: () => {return this.isLoadingPlayers = false, this.isLoadedPlayer = true}
-  //   });
-  // }
-  // }
-  ngOnInit() {
-    // this.receiveData()
+  //Programmate
+  isLoadingNext: boolean = true; // Flag per indicare se le partite sono in fase di caricamento
+  //Funzione di caricamento controlla lo stato della chiamata se restituisce o meno
+  loadingProgrammate(date:Date) {
+    this.isLoadingNext = true;
+    this.apiService.getGames(DateTime.fromJSDate(date).toFormat('yyyy-MM-dd')).subscribe({
+      next: (response) => {
+        this.nextGames = response;
+      },
+      error: (error) => console.error('Error fetching players', error),
+      complete: () => {return this.isLoadingNext = false}
+    });
   }
-
+  ngOnInit() {
+    this.loadingProgrammate(this.currentDate);
+  }
   
 }
